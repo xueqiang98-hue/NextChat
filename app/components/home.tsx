7d5dfb65980238cde6152cecf24683046a7378cf
xueqiang98-hue <xueqiang98@gmail.com>
2026-04-15 23:44:17 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -33,13 +33,16 @@ import { initializeMcpSystem, isMcpEnabled } from "../mcp/actions";
 
 export function Loading(props: { noLogo?: boolean }) {
   return (
-    <div style={{ padding: 10 }}>
-  <a href="/tools" style={{ marginRight: 10 }}>🛠 Tools</a>
-</div>
-    <div className={clsx("no-dark", styles["loading-content"])}>
-      {!props.noLogo && <BotIcon />}
-      <LoadingIcon />
-    </div>
+    <>
+      <div style={{ padding: 10 }}>
+        <a href="/tools" style={{ marginRight: 10 }}>🛠 Tools</a>
+      </div>
+
+      <div className={clsx("no-dark", styles["loading-content"])}>
+        {!props.noLogo && <BotIcon />}
+        <LoadingIcon />
+      </div>
+    </>
   );
 }
 
