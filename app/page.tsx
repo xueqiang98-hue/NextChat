b255a28629f95e69a774aadf724ff42c982bfffe
xueqiang98-hue <xueqiang98@gmail.com>
2026-04-15 23:05:32 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-<p style={{color: "red"}}>
-Free users: 10 messages per day. Upgrade for unlimited access.
-</p>
 import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 import { getServerSideConfig } from "./config/server";
@@ -10,7 +7,14 @@ const serverConfig = getServerSideConfig();
 export default async function App() {
   return (
     <>
+      <h1 style={{ color: "red" }}>TEST SUCCESS</h1>
+
+      <p style={{ color: "red" }}>
+        Free users: 10 messages per day. Upgrade for unlimited access.
+      </p>
+
       <Home />
+
       {serverConfig?.isVercel && (
         <>
           <Analytics />
@@ -19,4 +23,3 @@ export default async function App() {
     </>
   );
 }
-<h1 style={{color: "red"}}>TEST SUCCESS</h1>
